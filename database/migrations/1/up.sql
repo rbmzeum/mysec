badaebdfd2c9f89c2866bb1620e086b83dda0566
Viacheslav Shindin <t7gx98wk@gmail.com>
2024-01-13 03:25:40 +0700

    add migrations up and rollback

--- database/migrations/1/up.sql
+++ database/migrations/1/up.sql
@@ -1,1 +1,5 @@
-SELECT 4*4;
+CREATE TABLE IF NOT EXISTS persons (
+    id      SERIAL PRIMARY KEY,
+    name    TEXT NOT NULL,
+    data    BYTEA
+);
--- src/bin/migrations.rs
+++ src/bin/migrations.rs
@@ -3,8 +3,14 @@ use postgres_openssl::MakeTlsConnector;
 use std::sync::Arc;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use tokio::fs::try_exists;
 
 use secsak::modules::verify::Store as VirifyStore;
+
+const MS_STATUS_APPLIED: u8 = 0;
+const MS_STATUS_FAILED: u8 = 1;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -44,28 +50,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    client.batch_execute("
-        CREATE TABLE IF NOT EXISTS person (
-            id      SERIAL PRIMARY KEY,
-            name    TEXT NOT NULL,
-            data    BYTEA
-        )
-    ").await?;
-
-    client.batch_execute("
-        DROP TABLE person;
-    ").await?;
-
-    // 
-    // for argument in env::args() {
-    //     println!("{argument}");
-    // }
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
         match args[1].as_str() {
             "rollback" => {
                 println!("MIGRATION DOWN");
+
+                let mut migration_path = current_dir.clone();
+                migration_path.push("database/migrations");
+
+                let mut migrations = fs::read_dir(&migration_path)?.map(|v| v.unwrap().path()).collect::<Vec<_>>();
+                migrations.sort();
+                migrations.reverse();
+                for path in migrations {
+                    let mut status_path = path.clone();
+                    status_path.push("status");
+                    if try_exists(&status_path).await? {
+                        let mut file = File::open(&status_path)?;
+                        let mut status = [0; 1];
+                        file.read_exact(&mut status)?;
+                        if status[0] != MS_STATUS_APPLIED {
+                            continue;
+                        }
+                    }
+
+                    let mut down_path = path.clone();
+                    down_path.push("down.sql");
+
+                    let down_contents = fs::read_to_string(down_path)
+                        .expect("Should have been able to read the file down.sql");
+
+                    println!("Rollback SQL:\n{:#?}", &down_contents);
+                    client.batch_execute(down_contents.as_str()).await?;
+
+                    fs::remove_file(&status_path)?;
+                }
             },
             _ => {
                 println!("UNKNOWN PARAMENTER");
@@ -77,25 +97,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut migration_path = current_dir.clone();
         migration_path.push("database/migrations");
 
-        // TODO: отсортировать
-        for entry in fs::read_dir(migration_path)? {
-            let entry = entry?;
-            let path = entry.path();
+        let mut migrations = fs::read_dir(&migration_path)?.map(|v| v.unwrap().path()).collect::<Vec<_>>();
+        migrations.sort();
+        for path in migrations {
+            let mut status_path = path.clone();
+            status_path.push("status");
+            if try_exists(&status_path).await? {
+                let mut file = File::open(&status_path)?;
+                let mut status = [0; 1];
+                file.read_exact(&mut status)?;
+                if status[0] == MS_STATUS_APPLIED {
+                    continue;
+                }
+            }
 
             let mut up_path = path.clone();
             up_path.push("up.sql");
 
-            // let mut down_path = path.clone();
-            // down_path.push("down.sql");
-
             let up_contents = fs::read_to_string(up_path)
                 .expect("Should have been able to read the file up.sql");
-            // let down_contents = fs::read_to_string(down_path)
-            //     .expect("Should have been able to read the file down.sql");
 
             println!("SQL:\n{:#?}", &up_contents);
             client.batch_execute(up_contents.as_str()).await?;
-            // TODO: создать файл с информацией о выполнении up.sql (для того, чтобы при последующих запусках проигнорировать выполнение данной миграции)
+
+            let mut file = File::create(&status_path)?;
+            file.write_all(&[MS_STATUS_APPLIED])?;
         }
     }
 
