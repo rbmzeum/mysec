f7a450e37782f79e8c853f31df2c14ae38c5f0a5
Viacheslav Shindin <t7gx98wk@gmail.com>
2024-01-18 05:17:08 +0700

    implement batch_execute into db_store.actions

--- src/bin/migrations.rs
+++ src/bin/migrations.rs
@@ -15,7 +15,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let current_dir = std::env::current_dir().expect("failed to read current directory");
 
     let db_store = DbStore::new("host=localhost user=postgres sslmode=require dbname=mysec").await;
-    let client = db_store.getters.get_client();
 
     let args: Vec<String> = env::args().collect();
 
@@ -51,7 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         .expect("Should have been able to read the file down.sql");
 
                     println!("Rollback SQL:\n{:#?}", &down_contents);
-                    client.lock().unwrap().batch_execute(down_contents.as_str()).await?;
+                    db_store.actions.batch_execute(down_contents.as_str()).await?;
 
                     fs::remove_file(&status_path)?;
                 }
@@ -88,7 +87,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             println!("Path: {:#?}", &up_path);
             println!("SQL:\n{:#?}", &up_contents);
-            client.lock().unwrap().batch_execute(up_contents.as_str()).await?;
+            db_store.actions.batch_execute(up_contents.as_str()).await?;
 
             let mut file = File::create(&status_path)?;
             file.write_all(&[MS_STATUS_APPLIED])?;
--- src/modules/db/actions.rs
+++ src/modules/db/actions.rs
@@ -10,4 +10,10 @@ impl Actions {
     pub fn new(state: Arc<Mutex<State>>) -> Self {
         Actions { state }
     }
+
+    pub async fn batch_execute(&self, sql: &str) -> Result<(), tokio_postgres::Error> {
+        let mut state = self.state.lock().unwrap();
+        let result = state.client.lock().unwrap().batch_execute(sql).await;
+        result
+    }
 }
