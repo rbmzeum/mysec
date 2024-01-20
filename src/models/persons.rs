4dfd0942b394f580f3c584e1ecb4c3e1191b7e42
Viacheslav Shindin <t7gx98wk@gmail.com>
2024-01-20 08:36:37 +0700

    add struct PersonControl (uncompleted)

--- src/models/persons.rs
+++ src/models/persons.rs
@@ -1,4 +1,8 @@
 use crate::modules::db::Store;
+use crate::secure_invite::{
+    keypair::KeyPair,
+    // crypt::Crypt,
+};
 
 const ERR_PERSON_NOT_FOUND: u8 = 1;
 
@@ -15,6 +19,20 @@ impl std::fmt::Display for PersonError {
 }
 
 impl std::error::Error for PersonError {}
+
+unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
+    ::core::slice::from_raw_parts(
+        (p as *const T) as *const u8,
+        ::core::mem::size_of::<T>(),
+    )
+}
+
+pub struct PersonControl {
+    pub nonce: u64,
+    // pub control_sum: [u8; 56], // TODO: SHA-3 (Keccak) size
+    // pub parent_control_sum: [u8; 56],
+    // pub key_pair: KeyPair, // serde не подходит, надо другим способом
+}
 
 pub struct Person {
     pub id: i64,
@@ -55,7 +73,6 @@ impl Person {
             &self.version,
             &self.data, // TODO: make dynamically use trust_chain
         ]).await;
-        println!("Save result: {:#?}", &result);
         match result {
             Ok(_v) => true,
             Err(_e) => false,
@@ -83,11 +100,19 @@ mod tests {
     async fn test_save_find_delete() {
         let db_store = crate::modules::db::Store::new("host=localhost user=postgres sslmode=require dbname=mysec").await;
 
-        let p = Person{
+        let mut p = Person{
             id: -1,
             version: 1,
             data: vec![],
         };
+
+        let pc = PersonControl {
+            nonce: 34,
+        };
+
+        let bytes: &[u8] = unsafe { any_as_u8_slice(&pc) };
+        p.data = Vec::from(bytes); // TODO: добавлять через метод Person: p.make_control_data(parent_control_sum); // и спроектировать связь с trust_chain
+
         let res = p.save(&db_store).await;
         assert_eq!(res, true);
 
